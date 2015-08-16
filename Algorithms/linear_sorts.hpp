ec39e64895090c4594e94c4559693e398b2a8a71
Gabriel Ortega <gabriel.ortega56@gmail.com>
2015-08-15 23:24:55 -0400

    Cleaned up radix sort. Made counting sort assumptions more explicit by using uint32 as param type

--- Algorithms/linear_sorts.hpp
+++ Algorithms/linear_sorts.hpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <bitset>
 #include <climits>
+#include <stdint.h>
 
 namespace cliqCity
 {
@@ -19,98 +20,107 @@ namespace cliqCity
 		// Counting Sort: Uses auxiliary storage to count elements in array t. The counter maintains the number of elements in rank with each index. 
 		// The counter is then used to index back into the output array. Runs in linear time if max <= length^2.
 		template<class T, class R>
-		void countingSort(T* const t, T* const output, const R& rank, const int& max, const int& length, SortOrder order)
+		void uint32_CountingSort(T* const t, T* const output, const R& uint32_Rank, const uint32_t& max, const int& length, SortOrder order)
 		{
 			// Initialize counter and set to zero.
-			unsigned int* counter = new unsigned int[max + 1];
-			memset(counter, 0, sizeof(unsigned int) * (max + 1));
+			uint32_t* counter = new uint32_t[max + 1];
+			memset(counter, 0, sizeof(uint32_t) * (max + 1));
 
 			// Set counter[j] to number of elements equal to j
 			for (int j = 0; j < length; j++) {
-				counter[rank(t[j])]++;
+				counter[uint32_Rank(t[j])]++;
 			}
 
 			if (order == SortOrderDescending) {
 				// Set counter[i] to number of elements greater than or equal to j
-				for (int i = max - 1; i >= 0; i--) {
-					counter[i] += counter[i + 1];
+				uint32_t* iter = &counter[max - 1];
+				uint32_t* end = (counter - 1);
+				while (iter != end) {
+					*iter += *(iter + 1);
+					iter--;
 				}
 			}
 			else {
 				// Set counter[i] to number of elements less than or equal to j
-				for (int i = 1; i <= max; i++) {
-					counter[i] += counter[i - 1];
+				uint32_t* iter = &counter[1];
+				uint32_t* end = &counter[max + 1];
+				while (iter != end) {
+					*iter += *(iter - 1);
+					iter++;
 				}
 			}
 
 			// Index back into the output array
 			for (int j = length - 1; j >= 0; j--) {
-				int r = rank(t[j]);
+				uint32_t r = uint32_Rank(t[j]);
 				output[counter[r] - 1] = t[j];
 				counter[r]--;
 			}
 
 			delete[] counter;
 		}
-
-		typedef unsigned __int32	UInt32;
-		typedef unsigned char		UByte;
 		
 		template<class T, class K>
-		void radixSort(T* const t, T* const output, const K& uint32Key, const int& length, SortOrder order)
+		void uint32_RadixSort(T* const t, T* const output, const K& uint32_Key, const int& length, SortOrder order)
 		{
 			// Range * Number of Counters * Bytes Per Location
-			UInt32 counter[256 * 4];
-			memset(counter, 0, 256 * 4 * sizeof(UInt32));
+			uint32_t counter[256 * 4];
+			memset(counter, 0, 256 * 4 * sizeof(uint32_t));
 
-			UInt32* c0 = (UInt32*)&counter[0];		// LSB
-			UInt32* c1 = (UInt32*)&counter[256];
-			UInt32* c2 = (UInt32*)&counter[512];
-			UInt32* c3 = (UInt32*)&counter[768];	// MSB
+			uint32_t* c0 = &counter[0];		// LSB
+			uint32_t* c1 = &counter[256];
+			uint32_t* c2 = &counter[512];
+			uint32_t* c3 = &counter[768];	// MSB
 
 			// Counting Pass
-			UByte* byte = NULL;
+			uint8_t* byte = NULL;
 			for (int i = 0; i < length; i++) {
-				byte = (UByte*)uint32Key(t, i);
+				byte = (uint8_t*)uint32_Key(t, i);
 
+				// Split key into individual bytes and count in each list
 				c0[*byte]++;
 				c1[*(byte + 1)]++;
 				c2[*(byte + 2)]++;
 				c3[*(byte + 3)]++;
-
-				std::cout << *uint32Key(t, i) << " : " << std::bitset<8>(*byte) << "     " << std::bitset<8>(*(byte + 1)) << "     " << std::bitset<8>(*(byte + 2)) << "     " << std::bitset<8>(*(byte + 3)) << std::endl;
 			}
 
 			// Offset Pass
-			for (int i = 1; i < 256; i++) {
-				c0[i] += c0[i - 1];
-				c1[i] += c1[i - 1];
-				c2[i] += c2[i - 1];
-				c3[i] += c3[i - 1];
+			if (order == SortOrderDescending) {
+				// Set counter[i] to number of elements greater than or equal to I
+				for (int i = 254; i >= 0; i--) {
+					c0[i] += c0[i + 1];
+					c1[i] += c1[i + 1];
+					c2[i] += c2[i + 1];
+					c3[i] += c3[i + 1];
+				}
+			}
+			else {
+				// Set counter[i] to number of elements less than or equal to i
+				for (int i = 1; i < 256; i++) {
+					c0[i] += c0[i - 1];
+					c1[i] += c1[i - 1];
+					c2[i] += c2[i - 1];
+					c3[i] += c3[i - 1];
+				}
 			}
 
-			// Radix Passes
-			// Figure out the swap!!! Almost there!
-			T* swap = new T[length];
 			size_t size = sizeof(T) * length;
+			T* swap = new T[length];
 			memcpy_s(swap, size, t, size);
 
-			UInt32* c = NULL;
+			// Radix Passes
+			uint32_t* c = NULL;
 			for (int pass = 0; pass < 4; pass++) {
-				c = reinterpret_cast<UInt32*>(&counter[(pass << 8)]);
-				printf("Pass %i\n", pass + 1);
-				UInt32 *k = NULL;
+
+				c = &counter[(pass << 8)];
 				for (int tIndex = length - 1; tIndex >= 0; tIndex--) {
-					k = uint32Key(swap, tIndex);
-					UByte* radix = ((UByte*)k) + pass;
+					uint8_t* radix = ((uint8_t*)uint32_Key(swap, tIndex)) + pass;
 					output[c[*(radix)] - 1] = swap[tIndex];
-					printf("%i : %u\n", c[*(radix)] - 1, output[c[*(radix)] - 1]);
 					c[*(radix)]--;
 				}
 
+				// Copy output back to swap for next pass
 				memcpy_s(swap, size, output, size);
-
-				printf("\n");
 			}
 
 			delete[] swap;
